9c852dfc22d741d13a9d43ec61384b3a4b6b3553
Mekdim Tamirat <jrmekdim50@gmail.com>
2024-05-07 12:35:30 +0300

    feat: Add HeaderWidgetLabelOnly and HeaderWidgetWithButton components

---
+++ lib/components/nav_bar_header_label_only.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class HeaderWidgetLabelOnly extends StatelessWidget {
+
+  const HeaderWidgetLabelOnly(List z, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
