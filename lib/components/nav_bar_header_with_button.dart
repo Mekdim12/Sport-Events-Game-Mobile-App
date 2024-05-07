51f528a1121dca80dee5856ced1045c82c22e309
Mekdim Tamirat <jrmekdim50@gmail.com>
2024-05-07 14:52:40 +0300

    refactor: Update HeaderWidgetWithButton to use custom back button image

--- lib/components/nav_bar_header_with_button.dart
+++ lib/components/nav_bar_header_with_button.dart
@@ -4,8 +4,7 @@ class HeaderWidgetWithButton extends StatelessWidget {
   final backPage;
   final pageTitle;
 
-  const HeaderWidgetWithButton(this.backPage, this.pageTitle,
-      );
+  const HeaderWidgetWithButton(this.backPage, this.pageTitle, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,21 +25,32 @@ class HeaderWidgetWithButton extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            IconButton(
-              icon: const Icon(
-                Icons.arrow_back_ios,
-                color: Colors.white,
+            
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => backPage),
+                  );
+                },
+                child: Image.asset(
+                  'assets/images/back_button.png',
+                  width: 40,
+                  height: 40,
+                ),
               ),
-              onPressed: () {
-                Navigator.pushNamed(context, backPage);
-              },
             ),
-            Text(
-              pageTitle, // use pageTitle directly
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
+            const SizedBox(width: 35),
+            Expanded(
+              child: Text(
+                pageTitle, // use pageTitle directly
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ],
